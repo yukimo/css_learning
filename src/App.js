9fdbbde5fffda50dc6bf91693aa57cf9b9bc46b1
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-08-21 21:49:10 +0900

    css Modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import{InLineStyle}from"./compornents/InlineStyle";
+import { CssModules } from "./compornents/CssModules";
+import { InLineStyle } from "./compornents/InlineStyle";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <InLineStyle/>
-        </div>
+      <InLineStyle />
+      <CssModules />
+    </div>
   );
 }
--- src/compornents/InlineStyle.jsx
+++ src/compornents/InlineStyle.jsx
@@ -1,27 +1,27 @@
-export const InLineStyle=()=>{
-  const containerStyle={
-    border:"solid 2px #392eff",
-    borderRadius:"20px",
-    padding:"8px",
-    margine:"9px",
-    display:"flex",
-    justifyContent:"space-around",
-    alignItems:"center",
+export const InLineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "9px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
   };
-  const titleStyle={
-    margine:0,
-    color:"#3d84a8",
+  const titleStyle = {
+    margin: 0,
+    color: "#3d84a8"
   };
-  const buttonStyle={
-    backgroundColor:"#abedd8",
-    border:"none",
-    padding:"8px",
-    corderRadius:"8px",
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
   };
-  return(
+  return (
     <div style={containerStyle}>
-    <p style={titleStyle}> InLine Style</p>
-    <button style={buttonStyle}>がんば！</button>
+      <p style={titleStyle}> InLine Style</p>
+      <button style={buttonStyle}>がんば！</button>
     </div>
-  )
-}
+  );
+};
